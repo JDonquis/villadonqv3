bc42d9472c91a27c837cb527d896c8c2c44a4a0c
juanvillans <juanvillans16@gmail.com>
2026-08-23 16:21:48 -0400

    ni

--- resources/js/components/LeftNav.svelte
+++ resources/js/components/LeftNav.svelte
@@ -52,7 +52,7 @@
         class=" text-lg  md:block p-4 text-center w-full  flex"
         >
         <img src="/img/Isotipo-villadonq-blanco.png" alt="Villadonq" class="w-10 h-10 object-contain inline" />
-        <span class="logo">
+        <span class="logo hidden md:inline-block">
             VILLADONQ
         </span>
         </a
